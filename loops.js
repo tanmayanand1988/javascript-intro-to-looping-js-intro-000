490aedd52401d69da545cc87691ce9be1ae7137c
Tanmay Anand <tanmayanand@gmail.com>
2020-06-28 19:59:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,13 +1,12 @@
-function forLoop(array){
-    for (let i = 0;i<25;i+=1){
-        if(i==1){
-            array[i]="I am 1 strange loop.";
-        }
-        else{
-            array[i]=`I am ${i} strange loops.`;
-        }
-    }
-    return array;
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
 
 function whileLoop(number){
